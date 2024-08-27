d00b52ea238f2e0d12fc6859b0952376bd4eb26b
uwi0 <kakapoweb@gmail.com>
2024-08-28 03:30:29 +0700

    Initial Commit

---
+++ Drag Image From Above/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Drag Image From Above
+//
+//  Created by dwi prasetyo on 28/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Drag Image From Above/Drag_Image_From_AboveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drag_Image_From_AboveApp.swift
+//  Drag Image From Above
+//
+//  Created by dwi prasetyo on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Drag_Image_From_AboveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
