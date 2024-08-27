6fe7afcbe2acf57cce543ce93d9e4c33b31a1289
uwi0 <kakapoweb@gmail.com>
2024-08-28 04:37:22 +0700

    Drag Image From Above Part 2

--- Drag Image From Above/ImageView.swift
+++ Drag Image From Above/ImageView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct ImageView: View {
+    
     @Binding var image: ImageModel
     @State var dragAmount = CGSize.zero
     @State var isTapped: Bool = false
-    let returnDUration: Double = 0.3
     
+    let returnDuration: Double = 0.3
+    
+    var isDragging: Bool {
+        dragAmount != .zero
+    }
+    var drag: some Gesture {
+        DragGesture(coordinateSpace: .global)
+            .onChanged { value in
+                withAnimation {
+                    dragAmount = CGSize(
+                        width: value.translation.width,
+                        height: value.translation.height
+                    )
+                    
+                    image = ImageModel(name: image.name, color: image.color, zIndex: 1.0)
+                }
+            }
+            .onEnded{ value in
+                withAnimation {
+                    dragAmount = .zero
+                    
+                    DispatchQueue.main.asyncAfter(deadline: .now() + returnDuration){
+                        image = ImageModel(
+                            name: image.name,
+                            color: image.color
+                        )
+                    }
+                }
+            }
+    }
     
     var body: some View {
         
@@ -22,23 +52,56 @@ struct ImageView: View {
             endPoint: .bottomTrailing
         )
         
-        ZStack {
-            Image(systemName: image.name)
-                .imageScale(.large)
-                .foregroundColor(image.color)
-                .padding()
-            .background(RoundedRectangle(cornerRadius: 10).fill(gradient))
-            
-            if(dragAmount != .zero) {
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(image.color, lineWidth: 3)
-                    .blur(radius: 5)
+        
+        Image(systemName: image.name)
+            .imageScale(.large)
+            .foregroundColor(image.color)
+            .padding()
+            .background(ZStack {
+                RoundedRectangle(cornerRadius: 10).fill(gradient)
+                
+                if(dragAmount != .zero) {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(image.color, lineWidth: 3)
+                        .blur(radius: 5)
+                }
+            })
+            .opacity(isDragging ? 0.9 : 1.0)
+            .scaleEffect(isDragging ? 1.2 : 1.0)
+            .offset(dragAmount)
+            .scaleEffect(isTapped ? 1.1 : 1.0)
+            .animation(.spring(response: 0.4, dampingFraction: 0.6), value: isTapped)
+            .gesture(drag)
+            .onTapGesture {
+                isTapped = true
+                image = ImageModel(name: image.name)
+                DispatchQueue.main.asyncAfter(deadline: .now() + returnDuration) {
+                    isTapped = false
+                }
             }
-        }
+        
+        
+        
+        
     }
     
 }
 
 #Preview {
-    ImageView(image: .constant(.init(name: "menucard.fill", color: .red)))
+    ZStack {
+        LinearGradient(
+            colors: [.blue, .purple],
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing)
+        .opacity(0.7)
+        .ignoresSafeArea()
+        VStack {
+            Text("Hello world").foregroundStyle(.white)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            Spacer()
+            ImageView(image: .constant(.init(name: "menucard.fill", color: .red)))
+            Spacer()
+        }
+    }
 }
